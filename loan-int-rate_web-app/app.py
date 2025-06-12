f084720e901f806ec0038958b021c791f9d9cd4a
devern-DZZC <devern.chattergoon@my.uwi.edu>
2025-06-12 00:56:53 -0400

    added signup and login page

--- loan-int-rate_web-app/app.py
+++ loan-int-rate_web-app/app.py
@@ -1,6 +1,128 @@
+import os, csv
+import datetime
+from flask import Flask, request, redirect, render_template, url_for, flash, jsonify
+from flask_cors import CORS
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    set_access_cookies,
+    unset_jwt_cookies,
+    current_user
+)
 import joblib
 import pandas as pd
 
+from models import db, User
+
+# Configure Flask App
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MySecretKey'
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
+app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
+app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(hours=15)
+app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+app.config['JWT_HEADER_NAME'] = "Cookie"
+
+
+# Initialize App 
+db.init_app(app)
+app.app_context().push()
+CORS(app)
+jwt = JWTManager(app)
+
+# JWT Config to enable current_user
+@jwt.user_identity_loader
+def user_identity_lookup(identity):
+  return identity
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+  identity = jwt_data["sub"]
+  return User.query.get(str(identity))
+
+def login_user(username, password):
+  user = User.query.filter_by(username=username).first()
+  if user and user.check_password(password):
+    token = create_access_token(identity=str(user.id))
+    return token
+  return None
+
+def initialize_db():
+  db.drop_all()
+  db.create_all()
+  user = User(username='bob', password='bobpass')
+  db.session.add(user)
+  db.session.commit()
+
+@app.route("/", methods=['GET'])
+def login_page():
+  return render_template("login.html")
+
+@app.route("/signup", methods=['POST'])
+def signup_action():
+  response = None
+  try:
+    username = request.form['username']
+    password = request.form['password']
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    response = redirect(url_for('home_page'))
+    token = create_access_token(identity=str(user.id))
+    set_access_cookies(response, token)
+  except IntegrityError:
+    flash('Username already exists')
+    response = redirect(url_for('signup_page'))
+  flash('Account created')
+  return response
+
+@app.route("/logout", methods=['GET'])
+@jwt_required()
+def logout_action():
+  response = redirect(url_for('login_page'))
+  unset_jwt_cookies(response)
+  flash('Logged out')
+  return response
+
+# *************************************
+
+# Page Routes (To Update)
+
+@app.route("/app", methods=['GET'])
+@jwt_required()
+def home_page():
+    return render_template(
+        "index.html", 
+        current_user=current_user, 
+    )
+
+
+@app.route("/login", methods=['POST'])
+def login_action():
+  # implement login
+  data = request.form
+  token = login_user(data['username'], data['password'])
+  response = None
+  print(token)
+  if token:
+    flash('Logged in successfully!')
+    response = redirect(url_for('home_page'))
+    set_access_cookies(response, token)
+  else:
+    flash('Incorrect username or password.')
+    response = redirect(url_for('login_page'))
+  return response
+
+
+
+""""
 print('Loading...')
 
 model = joblib.load('model/prediction.pkl')
@@ -33,3 +155,8 @@ pred = model.predict(transformed_X)
 
 
 print("Prediction:", pred)
+
+"""
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080)
