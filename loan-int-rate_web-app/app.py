aba39094327ab6ef71b03e4868ef51631fc31eae
devern-DZZC <devern.chattergoon@my.uwi.edu>
2025-06-13 15:15:02 -0400

    config

--- loan-int-rate_web-app/app.py
+++ loan-int-rate_web-app/app.py
@@ -23,15 +23,15 @@ print('Model Loaded!')
 
 # Configure Flask App
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(app.root_path, 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'MySecretKey'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_secret_key')
 app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
 app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
 app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(hours=15)
 app.config["JWT_COOKIE_SECURE"] = True
-app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY', 'fallback_jwt_secret')
 app.config["JWT_COOKIE_CSRF_PROTECT"] = False
 app.config['JWT_HEADER_NAME'] = "Cookie"
 
